aac4b9c2f16db0ece8d2f249e59c574d44eabd47
Arturo Guerrero <arturo.guerrero.g@gmail.com>
2023-07-31 01:17:26 -0500

    implementacion de rutas

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,118 +1,30 @@
-import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
-  const HomeScreen({super.key});
+
+  final int pageIndex;
+  const HomeScreen({
+    super.key,
+    required this.pageIndex,
+  });
+
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    SizedBox(),
+    FavoritesView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
-    );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final initialLoading = ref.watch(initialLoadingProvider);
-    if(initialLoading) return FullScreenLoader();
-    final slideShowMovies = ref.watch(moviesSlideshowProvider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    //if(nowPlayingMovies.length == 0) return CircularProgressIndicator();
-
-    return CustomScrollView(slivers: [
-      const SliverAppBar(
-        floating: true,
-        flexibleSpace: FlexibleSpaceBar(
-          titlePadding: EdgeInsets.zero,
-          title: CustomAppBar(),
-          centerTitle: false,
-        ),
+    return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
       ),
-      SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-        return Column(
-          children: [
-            //const CustomAppBar(),
-
-            MoviesSlideShow(movies: slideShowMovies),
-            MovieHorizontalListView(
-              movies: nowPlayingMovies,
-              title: 'En cines',
-              subTitle: 'Lunes 20',
-              loadNextPage: () {
-                ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-
-            MovieHorizontalListView(
-              movies: upcomingMovies,
-              title: 'Proximamente',
-              // subTitle: 'Este mes',
-              loadNextPage: () {
-                ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-
-            MovieHorizontalListView(
-              movies: popularMovies,
-              title: 'Populares',
-              // subTitle: 'Este mes',
-              loadNextPage: () {
-                ref.read(popularMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-
-            MovieHorizontalListView(
-              movies: topRatedMovies,
-              title: 'Mejor calificadas',
-              subTitle: 'De todos los tiempos',
-              loadNextPage: () {
-                ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-
-            const SizedBox(height: 10)
-
-            // Expanded(
-            //   child: ListView.builder(
-            //     itemCount: nowPlayingMovies.length,
-            //     itemBuilder: (context, index) {
-            //       final movie = nowPlayingMovies[index];
-            //       return ListTile(
-            //         title: Text(movie.title),
-            //       );
-            //     },
-            //   ),
-            // )
-          ],
-        );
-      }, childCount: 1))
-    ]);
+      bottomNavigationBar: CustomBottomNavigation(currentIndex: pageIndex),
+    );
   }
 }
